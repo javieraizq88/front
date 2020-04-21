7e2dc98f8eefa2b989e7281b9c6d8e03bc21449a
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-04-21 17:54:34 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import NavBar from './components/navBar.jsx'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import Login from './views/login.jsx'
+import Register from './views/register.jsx'
+import Profile from './views/profile.jsx'
+import About from './views/About.jsx'
+import OurTeam from './views/OurTeam.jsx'
+import Home from './views/Home.jsx'
+import Nutritionists from './components/nutritionists.jsx'
+import PersonalTrainers from './components/personaltrainers.jsx'
+import NutritionalEducation from './components/nutrionaleducation.jsx'
+import FitnessEducation from './components/fitnesseducation.jsx'
+import HealthTeam from './components/healthteam.jsx'
+import HealthPlans from './components/healthplans.jsx'
+import ProfessionalProfile from './views/professionalprofile.jsx'
+import Clients from './components/clients.jsx'
+import injectContext from './store/appContext.js'
+import AdminLogin from './views/adminlogin.jsx'
+import AdminProfile from './views/adminprofile.jsx'
+import AdminClients from './views/AdminClients.jsx'
+import AdminProfesionals from './views/AdminProfesionals.jsx'
+import ClientDetail from './views/AdminClientDetail.jsx'
+import FormIngresoClient from './components/formingresocliente.jsx'
+import ClientPlans from './components/clientPlans.jsx'
+import AvisoEnvioFormClient from './views/avisoenvioformcliente.jsx'
+
+
+const App = props => {
+
+  return (
+    <>
+      <Router>
+      <NavBar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/team' component={OurTeam} />
+          <Route path='/nutrionists' component={Nutritionists}/>
+          <Route path='/personal-trainers' component={PersonalTrainers}/>
+          <Route path='/nutritional-education' component={NutritionalEducation} />
+          <Route path='/fitness-education' component={FitnessEducation} />
+          <Route path='/about' component={About} />
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+          <Route path='/profile/health-team' component={HealthTeam} />
+          <Route path='/profile/health-plans' component={HealthPlans} />
+          <Route exact path='/profile/professional/clients' component={Clients} />
+          <Route exact path='/profile/professional' component={ProfessionalProfile} />
+          <Route exact path='/profile' component={Profile} />
+          <Route exact path='/profile/client/plans' component={ClientPlans} />         
+          <Route exact path="/profile/fichaingreso" component={FormIngresoClient} />
+          <Route exact path="/profile/envioficha" component={AvisoEnvioFormClient} />
+          <Route exact path='/admin/login' component={AdminLogin} />
+          <Route exact path='/admin/profile' component={AdminProfile} />
+          <Route exact path='/admin/client' component={AdminClients} />
+          <Route exact path='/admin/client/:id' component={ClientDetail} />
+          <Route exact path='/admin/profesionals' component={AdminProfesionals} />
+        </Switch>
+      </Router>
+    </>
+
+  )
+}
+
+export default injectContext(App);
---
+++ src/store/flux.js
@@ -0,0 +1,171 @@
+const getState = ({ getStore, getActions, setStore }) => {
+    return {
+        store: {
+            path: 'http://localhost:5000',
+            username: null,
+            currentUser: null,
+            isAuthenticated: false,
+            email: null,
+            password: null,
+            name: null,
+            lastname: null,
+            error: null,
+            photo: null,
+            role: null,
+            clients: null
+        },
+        actions: {
+            handleChange: e => {
+                setStore({
+                    [e.target.name]: e.target.value
+                })
+            },
+            isAuthenticated: () => {
+                if (sessionStorage.getItem('currentUser')) {
+                    setStore({
+                        currentUser: JSON.parse(sessionStorage.getItem('currentUser')),
+                        isAuthenticated: sessionStorage.getItem('isAuthenticated')
+                    })
+                }
+            },
+            login: (e, history, role_id) => {
+                e.preventDefault()
+                const store = getStore();
+                fetch(store.path + '/login/' + role_id, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        email: store.email,
+                        password: store.password
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                    .then(resp => resp.json())
+                    .then(data => {
+                        if (data.msg) {
+                            setStore({
+                                error: data
+                            })
+                        } else {
+                            setStore({
+                                currentUser: data,
+                                isAuthenticated: true,
+                                email: null,
+                                password: null,
+                                error: null,
+                                role: null
+                            })
+                            sessionStorage.setItem('currentUser', JSON.stringify(data))
+                            sessionStorage.setItem('isAuthenticated', true)
+                            history.push('/profile')
+                        }
+                    })
+            },
+            
+
+            admin_login: (e, history) => {
+                e.preventDefault()
+                const store = getStore();
+                fetch(store.path + '/admin/login', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        username: store.username,
+                        password: store.password
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                    .then(resp => resp.json())
+                    .then(data => {
+                        if (data.msg) {
+                            setStore({
+                                error: data
+                            })
+                        } else {
+                            setStore({
+                                currentUser: data,
+                                isAuthenticated: true,
+                                username: null,
+                                email: null,
+                                password: null,
+                                error: null,
+                                role: null
+                            })
+                            sessionStorage.setItem('currentUser', JSON.stringify(data))
+                            sessionStorage.setItem('isAuthenticated', true)
+                            history.push('/admin/profile')
+                        }
+                    })
+            },
+
+            adminLoadClients: (id = '') => {
+                const store = getStore()
+                fetch(store.path +  id, {
+                    method: 'GET',
+                    headers: {
+                        "Content-type": 'aplication/json'
+                    }
+                })
+                    .then(resp => resp.json())
+                    .then(data => {
+                        if (data.msg) {
+                            setStore({
+                                error: data
+                            })
+                        } else {
+                            setStore({
+                                error:null,
+                                clients:data
+                            })
+                        }
+                    })
+            },
+            register_client: (e, history) => {
+                e.preventDefault();
+                const store = getStore();
+                let formData = new FormData()
+                formData.append('email', store.email)
+                formData.append('password', store.password)
+                formData.append('name', store.name)
+                formData.append('lastname', store.lastname)
+                fetch(store.path + '/register/client', {
+                    method: 'POST',
+                    body: formData
+                })
+                    .then(resp => resp.json())
+                    .then(data => {
+                        if (data.msg) {
+                            setStore({
+                                error: data
+                            })
+                        } else {
+                            setStore({
+                                currentUser: data,
+                                isAuthenticated: true,
+                                email: null,
+                                password: null,
+                                photo: null,
+                                error: null,
+                                role: null
+                            })
+                            sessionStorage.setItem('currentUser', JSON.stringify(data))
+                            sessionStorage.setItem('isAuthenticated', true)
+                            history.push('/profile')
+                        }
+                    })
+            },
+            // logout: (history) => {
+            //     setStore({
+            //         currentUser: null,
+            //         isAuthenticated: false,
+            //     });
+            //     sessionStorage.removeItem('currentUser');
+            //     sessionStorage.removeItem('isAuthenticated');
+            //     // history.push('/)
+            // }
+        }
+    }
+}
+export default getState;
